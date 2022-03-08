1a8c960d3859484ea98d4808e1cded07a5b2006c
9Hadzo9 <harunhadzo99@gmail.com>
2022-03-08 12:23:05 +0100

    Initial Commit

---
+++ Fondacija/FondacijaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FondacijaApp.swift
+//  Fondacija
+//
+//  Created by Harun on 8. 3. 2022..
+//
+
+import SwiftUI
+
+@main
+struct FondacijaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
